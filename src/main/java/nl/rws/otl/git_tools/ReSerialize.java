b8dc9e7c2f7dd5d58c9abe056887169e50f3830b
Rob van de Werken <rob.vande.werken@rws.nl>
2020-05-28 09:13:24 +0200

    Add gitignore do an in-place replacement of the output better error-handling

--- src/main/java/nl/rws/otl/git_tools/ReSerialize.java
+++ src/main/java/nl/rws/otl/git_tools/ReSerialize.java
@@ -11,24 +11,30 @@ import java.nio.file.Paths;
 
 @Slf4j
 public class ReSerialize {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if(args.length != 1){
-            throw new RuntimeException("This application requires exactly 1 parameter, namely the file to reserialize");
+            log.error("This application requires exactly 1 parameter, namely the file to reserialize");
+            return;
         }
         Path filePath = Paths.get(args[0]);
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 
         FileDocumentSource fileDocumentSource = new FileDocumentSource(filePath.toFile());
         CustomOntologyLoaderConfiguration customOntologyLoaderConfiguration = new CustomOntologyLoaderConfiguration();
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(fileDocumentSource, customOntologyLoaderConfiguration);
 
-        //Files.move(filePath, filePath.resolveSibling(filePath.getFileName()+".old"));
-        manager.saveOntology(ontology, new TurtleDocumentFormat(), IRI.create(filePath.toUri()+".new.ttl"));
+        try {
+            OWLOntology ontology = manager.loadOntologyFromOntologyDocument(fileDocumentSource, customOntologyLoaderConfiguration);
+            manager.saveOntology(ontology, new TurtleDocumentFormat(), IRI.create(filePath.toUri()));
+        } catch (OWLOntologyStorageException e) {
+            log.error("Failed to save: {}", e.getMessage());
+        } catch (OWLOntologyCreationException e) {
+            log.error("Failed to load: {}",e.getMessage());
+        }
     }
 
     public static class CustomOntologyLoaderConfiguration extends OWLOntologyLoaderConfiguration {
         @Override
-        public boolean isIgnoredImport(IRI iri) {
+        public boolean isIgnoredImport(@SuppressWarnings("NullableProblems") IRI iri) {
             // Always ignore imports
             return true;
         }
