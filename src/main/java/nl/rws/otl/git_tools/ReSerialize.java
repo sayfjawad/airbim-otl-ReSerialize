1786e55980bf00f010369af095f6096660213be4
Rob van de Werken <rob.vande.werken@rws.nl>
2020-06-02 12:03:02 +0200

    Add command-line options Add unit-tests with test-files

--- src/main/java/nl/rws/otl/git_tools/ReSerialize.java
+++ src/main/java/nl/rws/otl/git_tools/ReSerialize.java
@@ -1,35 +1,79 @@
 package nl.rws.otl.git_tools;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
 import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.model.*;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Slf4j
 public class ReSerialize {
-    public static void main(String[] args) {
-        if(args.length != 1){
-            log.error("This application requires exactly 1 parameter, namely the file to reserialize");
-            return;
-        }
-        Path filePath = Paths.get(args[0]);
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+    private static final String OPTION_INPUT = "f";
+    private static final String OPTION_OUTPUT = "o";
+    private static final String OPTION_REPLACE = "r";
 
+    private static final int STATUS_CHANGED = 1;
+    private static final int STATUS_ERROR = 2;
+
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addRequiredOption(OPTION_INPUT, "file", true, "File to validate");
+        options.addOption(OPTION_REPLACE, "replace", false, "Replace the file with the re-serialized version");
+        options.addOption(OPTION_OUTPUT, "output", true, "File to write with the re-serialized version");
+
+        CommandLine cmdArgs = null;
+        try {
+            cmdArgs = new DefaultParser().parse(options, args, true);
+        } catch (ParseException e) {
+            new HelpFormatter().printHelp("ReSerialize", options, true);
+            System.exit(STATUS_ERROR);
+        }
+
+        Path filePath = Paths.get(cmdArgs.getOptionValue(OPTION_INPUT));
         FileDocumentSource fileDocumentSource = new FileDocumentSource(filePath.toFile());
         CustomOntologyLoaderConfiguration customOntologyLoaderConfiguration = new CustomOntologyLoaderConfiguration();
 
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         try {
+            String inputHash = getHash(new FileInputStream(filePath.toFile()));
+
             OWLOntology ontology = manager.loadOntologyFromOntologyDocument(fileDocumentSource, customOntologyLoaderConfiguration);
-            manager.saveOntology(ontology, new TurtleDocumentFormat(), IRI.create(filePath.toUri()));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            manager.saveOntology(ontology, new TurtleDocumentFormat(), output);
+            byte[] outputBytes = output.toByteArray();
+            String outputHash = getHash(new ByteArrayInputStream(outputBytes));
+
+            if (!inputHash.equals(outputHash)){
+                log.info("ReSerialized version is different from input.");
+
+                if(cmdArgs.hasOption(OPTION_REPLACE)) {
+                    writeFile(outputBytes, filePath.toFile());
+                } else if(cmdArgs.hasOption(OPTION_OUTPUT)) {
+                    writeFile(outputBytes, new File(cmdArgs.getOptionValue(OPTION_OUTPUT)));
+                }
+
+                System.exit(STATUS_CHANGED);
+            }
+        } catch (IOException | OWLOntologyCreationException e) {
+            log.error("Failed to load: {}", e.getMessage());
+            System.exit(STATUS_ERROR);
         } catch (OWLOntologyStorageException e) {
             log.error("Failed to save: {}", e.getMessage());
-        } catch (OWLOntologyCreationException e) {
-            log.error("Failed to load: {}",e.getMessage());
+            System.exit(STATUS_ERROR);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Failed to use algorithm: {}", e.getMessage());
+            System.exit(STATUS_ERROR);
         }
+
+        System.exit(0);
     }
 
     public static class CustomOntologyLoaderConfiguration extends OWLOntologyLoaderConfiguration {
@@ -37,6 +81,29 @@ public class ReSerialize {
         public boolean isIgnoredImport(@SuppressWarnings("NullableProblems") IRI iri) {
             // Always ignore imports
             return true;
+        }
+    }
+
+    public static String getHash(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md;
+        try (DigestInputStream dis = new DigestInputStream(inputStream, MessageDigest.getInstance("SHA-256"))) {
+            //noinspection StatementWithEmptyBody
+            while (dis.read() != -1) {
+                //empty loop to clear the data
+            }
+            md = dis.getMessageDigest();
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    public static void writeFile(byte[] outputBytes, File file) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write(outputBytes);
         }
     }
 }
---
+++ src/test/java/nl/rws/otl/git_tools/ReSerializeTest.java
@@ -0,0 +1,97 @@
+package nl.rws.otl.git_tools;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+
+public class ReSerializeTest {
+    private static final URL testTtl = ReSerializeTest.class.getClassLoader().getResource("basicsemantics-owl.ttl");
+    private static final URL testTtlFixed = ReSerializeTest.class.getClassLoader().getResource("basicsemantics-owl_fixed.ttl");
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Test
+    public void testMainNoCommand() {
+        exit.expectSystemExitWithStatus(2);
+        String[] args = {};
+        ReSerialize.main(args);
+    }
+
+    @Test
+    public void testMainValid() {
+        exit.expectSystemExitWithStatus(0);
+        assertNotNull(testTtlFixed);
+        String[] args = {"-f",testTtlFixed.getPath()};
+        ReSerialize.main(args);
+    }
+
+    @Test
+    public void testMainDifferent() {
+        exit.expectSystemExitWithStatus(1);
+        assertNotNull(testTtl);
+        String[] args = {"-f",testTtl.getPath()};
+        ReSerialize.main(args);
+    }
+
+    @Test
+    public void testMainOutput() throws IOException {
+        exit.expectSystemExitWithStatus(1);
+        assertNotNull(testTtl);
+
+        Path tempFile = Files.createTempFile("unit_test_", ".ttl");
+        System.out.println("Creating testfile: "+tempFile.toString());
+
+        String[] args = {"-f",testTtl.getPath(),"-o",tempFile.toString()};
+        ReSerialize.main(args);
+
+        assertTrue(Files.deleteIfExists(tempFile));
+        System.out.println("Deleted testfile: "+tempFile.toString());
+    }
+
+    @Test
+    public void testMainReplace() throws IOException {
+        exit.expectSystemExitWithStatus(1);
+        assertNotNull(testTtl);
+
+        Path tempFile = Files.createTempFile("unit_test_", ".ttl");
+        Files.delete(tempFile);
+        Files.copy(new File(testTtl.getPath()).toPath(),tempFile);
+
+        System.out.println("Created testfile: "+tempFile.toString());
+        assertTrue(Files.exists(tempFile));
+
+        String[] args = {"-f",tempFile.toString(),"-r"};
+        ReSerialize.main(args);
+
+        assertTrue(Files.deleteIfExists(tempFile));
+        System.out.println("Deleted testfile: "+tempFile.toString());
+    }
+
+    @Test
+    public void testGetHash() throws IOException, NoSuchAlgorithmException {
+        assertNotNull(testTtl);
+        String hash = ReSerialize.getHash(testTtl.openStream());
+        assertEquals("76da850bba013f577570078580749c69364be5cb734816666b671bf8cca9f930",hash);
+    }
+
+    @Test
+    public void testWriteFile() throws IOException {
+        assertNotNull(testTtl);
+        Path tempFile = Files.createTempFile("unit_test_", ".ttl");
+        System.out.println("Creating testfile: "+tempFile.toString());
+        byte[] bytes = testTtl.openStream().readAllBytes();
+        ReSerialize.writeFile(bytes,tempFile.toFile());
+        assertTrue(Files.deleteIfExists(tempFile));
+        System.out.println("Deleted testfile: "+tempFile.toString());
+    }
+}
